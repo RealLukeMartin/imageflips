eee2dc815f8eb6f0af3fe8d98a26592b4fc09258
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-07-29 13:10:39 -0500

    add flip functionality

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 $(document).ready( function() {
 
 	$('.flip').click(function(){
-	    $(this).find('.card').addClass('flipped');
+	    $(this).find('.card').toggleClass('flipped');
 	    return false;
-	}).mouseleave(function () {
-	    $('.flip > .card').removeClass('flipped');
 	});
+	heightWidthSync();
+});
 
+$(window).on('resize', function() {
+	heightWidthSync();
+});
+
+function heightWidthSync() {
 	var frontHeight = $('.front').outerHeight();
 	var backHeight = $('.back').outerHeight();
 
 	if (frontHeight > backHeight) {
 	    $('.flip, .back').height(frontHeight);
 	}
-	else if (frontHeight > backHeight) {
+	else if (backHeight > frontHeight) {
 	    $('.flip, .front').height(backHeight);
 	}
 	else {
 	    $('.flip').height(backHeight);
 	}
-
-});
+}
