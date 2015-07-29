2734a9b172ebf7b1538a9ee895eba261a51ac28d
RealLukeMartin <lukemartinwebdev@gmail.com>
2015-07-29 10:12:16 -0500

    add basic card

--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
 $(document).ready( function() {
-	console.log('it works');
+
+	$('.flip').click(function(){
+	    $(this).find('.card').addClass('flipped');
+	    return false;
+	}).mouseleave(function () {
+	    $('.flip > .card').removeClass('flipped');
+	});
+
+	var frontHeight = $('.front').outerHeight();
+	var backHeight = $('.back').outerHeight();
+
+	if (frontHeight > backHeight) {
+	    $('.flip, .back').height(frontHeight);
+	}
+	else if (frontHeight > backHeight) {
+	    $('.flip, .front').height(backHeight);
+	}
+	else {
+	    $('.flip').height(backHeight);
+	}
+
 });
